cc1d1b5cf0eadfb8cbd5d0075d460124d3589ac2
jhonatan583 <jhoninhaoficial@gmail.com>
2025-03-10 21:09:38 -0300

    adicionando JS

---
+++ script.js
@@ -0,0 +1,14 @@
+const playhuman = (humanchoice) => {
+    console.log('retorno' + playmachine)
+   
+}
+
+
+const playmachine = () => {
+    const machine = ['stone', 'paper', 'scissors']
+    const randomnumber = Math.floor(Math.random() * 3)
+
+    return machine[randomnumber]
+
+
+}
