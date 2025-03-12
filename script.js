4994f7757e51d5fc674bfdb6d9047ed11162ff0e
jhonatan583 <jhoninhaoficial@gmail.com>
2025-03-12 17:42:26 -0300

    ajustando JS

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const result = (document.querySelector('.result'))
+ const humanScore=(document.querySelector("#human-score"))
+ const machineScore=(document.querySelector("#machine-score"))
 const playhuman = (humanchoice) => {
-    console.log(humanchoice)
-    console.log('retorno' + playmachine())
-   
+
+    playTheGame(humanchoice, playmachine())
 }
 
 
@@ -9,7 +11,25 @@ const playmachine = () => {
     const machine = ['stone', 'paper', 'scissors']
     const randomNumber = Math.floor(Math.random() * 3)
 
-    return machine[randomnumber]
 
+    return machine[randomNumber]
+
+}
+
+const playTheGame = (human, machine) => {
+
+    console.log('human:' + human + "maquina:" + machine)
+
+    if (human === machine) {
+        result.innerHTML = "Deu Empate!"
+    }
+    else if ((human === 'paper' && machine === 'stone') ||
+        (human === 'stone' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')) {
+        result.innerHTML = "Você Ganhou!"
+    }
+    else {
+        result.innerHTML = "Você Perdeu!"
+    }
 
 }
