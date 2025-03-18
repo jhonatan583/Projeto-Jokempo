ce4d9a3c1391e8788d2851fae4321273673ea7e9
jhonatan583 <jhoninhaoficial@gmail.com>
2025-03-18 05:09:28 -0300

    Testando ENUM

---
+++ javascript.js
@@ -0,0 +1,51 @@
+const result = document.querySelector('.result')
+const ScorePlayer = document.querySelector('#player-score')
+const ScoreMachine = document.querySelector('#machine-score')
+
+let ScorePlayerNumber = 0
+let ScoreMachineNumber = 0
+
+const GAME_OPTIONS={
+    STONE:'stone',
+    PAPER:'paper',
+    SCISSORS:'scissors'
+}
+const playhuman = (humanchoice) => {
+
+    playTheGame(humanchoice, playmachine())
+}
+
+
+const playmachine = () => {
+    const choices = [GAME_OPTIONS.STONE, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
+    const randomnumber = Math.floor(Math.random() * 3)
+
+    return choices[randomnumber]
+}
+
+const playTheGame = (human, machine) => {
+
+    console.log('humano :' + human + ' maquina :' + machine)
+
+    if (human === machine) {
+        result.innerHTML = "Deu Empate"
+
+    }
+    else if ((human === 'paper' && machine === 'stone')
+        || (human === 'stone' && machine === 'scissors')
+        || (human === 'scissors' && machine === 'paper')) {
+
+        ScorePlayerNumber++
+        ScorePlayer.innerHTML = ScorePlayerNumber
+
+
+        result.innerHTML = "Você Ganhou"
+
+
+    }
+    else {
+        ScoreMachineNumber++
+        ScoreMachine.innerHTML = ScoreMachineNumber
+        result.innerHTML = "Você Perdeu"
+    }
+}
